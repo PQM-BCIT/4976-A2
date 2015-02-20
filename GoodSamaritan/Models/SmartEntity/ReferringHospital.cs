7087d61279cf85b967800ddaf9b21ef7198f135e
cst-d3 <jchee12@my.bcit.ca>
2015-02-19 21:46:35 -0800

    Added SmartEntity Models

---
+++ GoodSamaritan/Models/SmartEntity/ReferringHospital.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models.SmartEntity
+{
+    public class ReferringHospital
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public List<SmartEntity> SmartEntity { get; set; }
+    }
+}
