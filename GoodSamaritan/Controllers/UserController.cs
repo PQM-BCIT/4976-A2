cebf4ed190827b4f56a447ab9a0416050a6f2853
Justin Chee <jchee12@my.bcit.ca>
2015-03-06 19:46:23 -0800

    Added locking of users

--- GoodSamaritan/Controllers/UserController.cs
+++ GoodSamaritan/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GoodSamaritan.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,8 +14,64 @@ namespace GoodSamaritan.Controllers
         // GET: User
         public ActionResult Index()
         {
+            
+            List<string> users;
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
 
-            return View();
+                users = (from u in userManager.Users select u.UserName).ToList();
+            }
+
+            return View(users);
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult UserLock(string userName)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = userManager.FindByName(userName);
+
+                user.LockoutEnabled = true;
+
+                DateTime dt = new DateTime(5000, 01, 01);
+                DateTimeOffset dto = new DateTimeOffset(dt);
+                userManager.SetLockoutEndDate(user.Id, dto);
+
+                context.SaveChanges();
+            }
+
+            ViewBag.ResultMessage = "User locked succesfully !";
+
+            return RedirectToAction("Index", "User");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public ActionResult UserUnlock(string userName)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = userManager.FindByName(userName);
+
+                user.LockoutEnabled = true;
+
+                DateTime dt = new DateTime(2000, 01, 01);
+                DateTimeOffset dto = new DateTimeOffset(dt);
+                userManager.SetLockoutEndDate(user.Id, dto);
+
+                context.SaveChanges();
+            }
+
+            ViewBag.ResultMessage = "User unlocked succesfully !";
+
+            return RedirectToAction("Index", "User");
         }
     }
 }
