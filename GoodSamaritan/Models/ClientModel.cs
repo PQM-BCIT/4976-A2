fbe62cc254af3edb9d5a0a2ff0a7ffaa7dcbd4db
Justin Chee <jchee12@my.bcit.ca>
2015-04-01 20:39:33 -0700

    No

--- GoodSamaritan/Models/ClientModel.cs
+++ GoodSamaritan/Models/ClientModel.cs
@@ -13,7 +13,6 @@ namespace GoodSamaritan.Models
         [Key]
         public int ClientReferenceNumber { get; set; }
 
-        [ForeignKey("FiscalYear")]
         public int FiscalYearId { get; set; }
         public FiscalYearModel FiscalYear { get; set; }
 
@@ -38,38 +37,30 @@ namespace GoodSamaritan.Models
         [Range(1, int.MaxValue, ErrorMessage = "Please enter a SWC File Number higher than 1")]
         public int SwcFileNumber { get; set; }
 
-        [ForeignKey("RiskLevel")]
         public int RiskLevelId { get; set; }
         public RiskLevelModel RiskLevel { get; set; }
 
-        [ForeignKey("Crisis")]
         public int CrisisId { get; set; }
         public CrisisModel Crisis { get; set; }
 
-        [ForeignKey("Service")]
         public int ServiceId { get; set; }
         public ServiceModel Service { get; set; }
 
-        [ForeignKey("Program")]
         public int ProgramId { get; set; }
         public ProgramModel Program { get; set; }
 
         [RegularExpression(@"^([a-zA-Z]+)$", ErrorMessage = "Please enter text only assessments")]
         public string RiskAssessmentAssignedTo { get; set; }
 
-        [ForeignKey("RiskStatus")]
         public int RiskStatusId { get; set; }
         public RiskStatusModel RiskStatus { get; set; }
 
-        [ForeignKey("AssignedWorker")]
         public int AssignedWorkerId { get; set; }
         public AssignedWorkerModel AssignedWorker { get; set; }
 
-        [ForeignKey("ReferralSource")]
         public int ReferralSourceId { get; set; }
         public ReferralSourceModel ReferralSource { get; set; }
 
-        [ForeignKey("Incident")]
         public int IncidentId { get; set; }
         public IncidentModel Incident { get; set; }
 
@@ -79,34 +70,27 @@ namespace GoodSamaritan.Models
         [RegularExpression(@"^[A-Z][a-z]*$", ErrorMessage = "Please enter a valid first name (eg. 'James')")]
         public string AbuserFirstName { get; set; }
 
-        [ForeignKey("AbuserRealtionship")]
         public int AbuserRelationshipId { get; set; }
         public AbuserRelationshipModel AbuserRealtionship { get; set; }
 
-        [ForeignKey("VictimOfIncident")]
         public int VictimOfIncidentId { get; set; }
         public VictimOfIncidentModel VictimOfIncident { get; set; }
 
-        [ForeignKey("FamilyViolenceFile")]
         public int FamilyViolenceFileId { get; set; }
         public FamilyViolenceFileModel FamilyViolenceFile { get; set; }
 
         [RegularExpression("^M|F|Trans$")]
         public char Gender { get; set; }
 
-        [ForeignKey("Ethnicity")]
         public int EthnicityId { get; set; }
         public EthnicityModel Ethnicity { get; set; }
 
-        [ForeignKey("Age")]
         public int AgeId { get; set; }
         public AgeModel Age { get; set; }
 
-        [ForeignKey("RepeatClient")]
         public int RepeatClientId { get; set; }
         public RepeatClientModel RepeatClient { get; set; }
 
-        [ForeignKey("DuplicateFile")]
         public int DuplicateFileId { get; set; }
         public DuplicateFileModel DuplicateFile { get; set; }
 
@@ -119,7 +103,6 @@ namespace GoodSamaritan.Models
         [Range(0, int.MaxValue, ErrorMessage = "Please enter a positive integer")]
         public int NumberOfChildren13To18 { get; set; }
 
-        [ForeignKey("StatusOfFile")]
         public int StatusOfFileId { get; set; }
         public StatusOfFileModel StatusOfFile { get; set; }
 
