329243a0397fdc771a75b17ac9d5178b166a15b6
Justin Chee <jchee12@my.bcit.ca>
2015-04-02 14:20:36 -0700

    Enabled Cors?

--- GoodSamaritan/App_Start/Startup.Auth.cs
+++ GoodSamaritan/App_Start/Startup.Auth.cs
@@ -6,11 +6,17 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using GoodSamaritan.Models;
+using GoodSamaritan.Providers;
+using Microsoft.Owin.Security.OAuth;
 
 namespace GoodSamaritan
 {
     public partial class Startup
     {
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static string PublicClientId { get; private set; }
+
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
@@ -36,6 +42,20 @@ namespace GoodSamaritan
                 }
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Configure the application for OAuth based flow
+            PublicClientId = "self";
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/Token"),
+                Provider = new ApplicationOAuthProvider(PublicClientId),
+                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true
+            };
+
+            // Enable the application to use bearer tokens to authenticate users
+            app.UseOAuthBearerTokens(OAuthOptions);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
             app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
---
+++ GoodSamaritan/Controllers/ClientAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using GoodSamaritan.Models;
+
+namespace GoodSamaritan.Controllers
+{
+    public class ClientAPIController : ApiController
+    {
+        private GoodSamaritanContext db = new GoodSamaritanContext();
+
+        // GET: api/ClientAPI
+        public IQueryable<ClientModel> GetClientModel()
+        {
+            return db.ClientModel;
+        }
+
+        // GET: api/ClientAPI/5
+        [ResponseType(typeof(ClientModel))]
+        public async Task<IHttpActionResult> GetClientModel(int id)
+        {
+            ClientModel clientModel = await db.ClientModel.FindAsync(id);
+            if (clientModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(clientModel);
+        }
+
+        // PUT: api/ClientAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutClientModel(int id, ClientModel clientModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != clientModel.ClientReferenceNumber)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(clientModel).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ClientAPI
+        [ResponseType(typeof(ClientModel))]
+        public async Task<IHttpActionResult> PostClientModel(ClientModel clientModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ClientModel.Add(clientModel);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = clientModel.ClientReferenceNumber }, clientModel);
+        }
+
+        // DELETE: api/ClientAPI/5
+        [ResponseType(typeof(ClientModel))]
+        public async Task<IHttpActionResult> DeleteClientModel(int id)
+        {
+            ClientModel clientModel = await db.ClientModel.FindAsync(id);
+            if (clientModel == null)
+            {
+                return NotFound();
+            }
+
+            db.ClientModel.Remove(clientModel);
+            await db.SaveChangesAsync();
+
+            return Ok(clientModel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClientModelExists(int id)
+        {
+            return db.ClientModel.Count(e => e.ClientReferenceNumber == id) > 0;
+        }
+    }
+}
