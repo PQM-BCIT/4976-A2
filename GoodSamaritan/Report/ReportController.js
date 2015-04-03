02119b8f7140cfe47f560e8e014d23bd417d89c3
Justin Chee <jchee12@my.bcit.ca>
2015-04-02 18:00:10 -0700

    Added styling.

--- GoodSamaritan/Report/ReportController.js
+++ GoodSamaritan/Report/ReportController.js
@@ -13,13 +13,13 @@
         $scope.years = [_year];
 
         // Grabs the fiscal years from the database
-        $http.get("../api/FiscalYearAPI") // will be changes to http://a3.thedistantvoice.me/api/FiscalYearAPI in the future..?
+        $http.get("../api/FiscalYearAPI") // will be changed to http://a3.thedistantvoice.me/api/FiscalYearAPI in the future..?
         .success(function (response) {
             $scope.years = response;
         });
 
         $scope.getClients = function () {
-            $http.get("../api/ClientAPI") // will be changes to http://a3.thedistantvoice.me/api/ClientAPI in the future..?
+            $http.get("../api/ClientAPI") // will be changed to http://a3.thedistantvoice.me/api/ClientAPI in the future..?
             .success(function (response) {
                 $scope.clients = response;
             });
