c4537caab75f39bfa02ae46cb28e6447181e62b6
Justin Chee <jchee12@my.bcit.ca>
2015-04-02 17:31:27 -0700

    Created dropdowns for month and fiscalyear

--- GoodSamaritan/Report/ReportController.js
+++ GoodSamaritan/Report/ReportController.js
@@ -5,12 +5,23 @@
     var ReportController = function ($scope, $http) {
 
         $scope.message = "Client Report";
+        
+        var _year = {
+            FiscalYearId: 0,
+            FiscalYear: "Loading..."
+        };
+        $scope.years = [_year];
+
+        // Grabs the fiscal years from the database
+        $http.get("../api/FiscalYearAPI") // will be changes to http://a3.thedistantvoice.me/api/FiscalYearAPI in the future..?
+        .success(function (response) {
+            $scope.years = response;
+        });
 
         $scope.getClients = function () {
-            $http.get("../api/ClientAPI")
+            $http.get("../api/ClientAPI") // will be changes to http://a3.thedistantvoice.me/api/ClientAPI in the future..?
             .success(function (response) {
                 $scope.clients = response;
-                alert(response);
             });
         }
     };
